f2ae3558d9e34d40a964837c396d33f14d8adb96
merienditas <maricersal@hotmail.com>
2022-02-23 14:09:14 +0100

    trabajando en adivinacion

--- Program.cs
+++ Program.cs
@@ -3,9 +3,42 @@ public class Program
 {
     public static void Main(String[] args)
     {
-        var generadorAleatorio = new Random();
-        int numeroParaAdivinar = generadorAleatorio.Next(0, 16);
+        const int respuesta = 0;
+        int numeroParaAdivinar = 0; 
 
-        Console.WriteLine($"El numero a adivinar era {numeroParaAdivinar}");
+    
+        while (respuesta <16)
+        {
+            MenuDeEntrada();
+            MenuIntentos();
+
+                if (numeroParaAdivinar <= 16 )
+                {
+
+                    var generadorAleatorio = new Random();
+                    numeroParaAdivinar = generadorAleatorio.Next(0, 16);
+
+                Console.WriteLine($"El número a adivinar era {numeroParaAdivinar}");
+                }
+      
+            break;
+        }
     }
+
+    public static void MenuDeEntrada ()
+    {
+        Console.WriteLine("Adivinación y otras cosas.  ");
+        Console.WriteLine("--------------------------");
+        Console.WriteLine ("Adivine en qué numero estoy pensando del 0 al 16. Tiene 3 intentos. ");
+        Console.WriteLine("--------------------------------------------------------------------");
+    }
+     private static void MenuIntentos()
+    {
+        Console.WriteLine("Introduzca un número. Éste es su primer intento. ");
+        int respuesta = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Éste es su segundo intento. ");
+        respuesta = Int32.Parse(Console.ReadLine());
+        Console.WriteLine("Éste es su tercer y último intento");
+        respuesta = Int32.Parse(Console.ReadLine());    
+    } 
 }
